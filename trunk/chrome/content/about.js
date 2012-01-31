71613ac8a81c7049c76e52acbeeba34ec8816f19
silkevicious <silkevicious@f9553a1c-787c-c2ac-8fa1-bce0d6f3e609>
2012-01-31 09:30:54 +0000

    Added "unpack" and some other things into manifest

--- trunk/chrome/content/about.js
+++ trunk/chrome/content/about.js
@@ -7,7 +7,7 @@ htlivesight.About = {
 	//	alert("3");	// loading xml file:			htlivesight.languageXML = htlivesight.loadXml(htlivesight.url);
 	//	alert("4");	// getting all the label:			htlivesight.data=htlivesight.languageXML.getElementsByTagName("Htlivesight");
 	//	alert("5");	// getting "htlivesight-about" label and setting it.			document.getElementById("htlivesight-about").attributes.getNamedItem("title").value=htlivesight.Util.Parse("WindowAboutTitle",htlivesight.data[0]);
-	//	alert("6");		// here there is date of creation, change here		document.getElementById("creationDate").value="13 "+htlivesight.Util.Parse("MonthJanuary",htlivesight.data[0])+" 2012";
+	//	alert("6");		// here there is date of creation, change here		document.getElementById("creationDate").value="31 "+htlivesight.Util.Parse("MonthJanuary",htlivesight.data[0])+" 2012";
 	//	alert("7");		// here there are authors name, change them here 		document.getElementById("createdBy").value=htlivesight.Util.Parse("TextCreatedBy",htlivesight.data[0])+" Silkevicious & Bigpapy";
 	//	alert("8");  }
 };
