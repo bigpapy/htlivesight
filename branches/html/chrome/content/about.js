0f67bd8cacc69c767c495075b149b7f3e6171dca
biigpapy@gmail.com <biigpapy@gmail.com@f9553a1c-787c-c2ac-8fa1-bce0d6f3e609>
2012-02-12 18:47:00 +0000

    Solved html passage issues for about.html.

--- branches/html/chrome/content/about.js
+++ branches/html/chrome/content/about.js
@@ -8,28 +8,28 @@ htlivesight.About = {
   },
   
   localization: function() {
-	  alert("1");
+	//  alert("1");
 	// loading preferences:
 		htlivesight.prefs=htlivesight.Preferences.get();
-//		alert("2");
+	//	alert("2");
 	// composition of the url about localization xml file: 
 		htlivesight.url = htlivesight.About.contentPath+"locale/"+ htlivesight.prefs.language.locale +".xml";
-//		alert("3");
+	//	alert("3");
 	// loading xml file:	
 		htlivesight.languageXML = htlivesight.loadXml(htlivesight.url);
-//		alert("4");
+	//	alert("4");
 	// getting all the label:	
 		htlivesight.data=htlivesight.languageXML.getElementsByTagName("Htlivesight");
-//		alert("5");
+	//	alert("5");
 	// getting "htlivesight-about" label and setting it.	
-		document.getElementById("htlivesight-about").attributes.getNamedItem("title").value=htlivesight.Util.Parse("WindowAboutTitle",htlivesight.data[0]);
-//		alert("6");
+	//	document.getElementById("htlivesight-about").attributes.getNamedItem("title").value=htlivesight.Util.Parse("WindowAboutTitle",htlivesight.data[0]);
+	//	alert("6");
 		// here there is date of creation, change here
-		document.getElementById("creationDate").value="13 "+htlivesight.Util.Parse("MonthJanuary",htlivesight.data[0])+" 2012";
+	//	document.getElementById("creationDate").value="13 "+htlivesight.Util.Parse("MonthJanuary",htlivesight.data[0])+" 2012";
 //		alert("7");
-		// here there are authors name, change them here 
-		document.getElementById("createdBy").innerHTML=/*htlivesight.Util.Parse("TextCreatedBy",htlivesight.data[0])+*/" Silkevicious & Bigpapy";
-//		alert("8");
+		// here there are authors name, change them here innerHTML
+		document.getElementById("createdBy").innerHTML=htlivesight.Util.Parse("TextCreatedBy",htlivesight.data[0]);
+	//	alert("8");
   }
 
 };
