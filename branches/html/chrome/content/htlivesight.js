8c950e8170f87c07d81c68e1e583ee1b776d3f81
biigpapy@gmail.com <biigpapy@gmail.com@f9553a1c-787c-c2ac-8fa1-bce0d6f3e609>
2012-02-13 20:31:42 +0000

    Changed button and menu.

--- branches/html/chrome/content/htlivesight.js
+++ branches/html/chrome/content/htlivesight.js
@@ -126,7 +126,7 @@ var htlivesight = {
   Options: function() {
 //	  alert("startup:Options begin");
     var features = "chrome,titlebar,toolbar,centerscreen,modal,resizable";
-    var url = htlivesightEnv.contentPath+"settings.html";
+    var url = htlivesightEnv.contentPath+"settings.xul";
     window.openDialog(url, "Options", features);
     htlivesight.prefs = htlivesight.Preferences.get();
 //    alert("startup:Options end");
@@ -134,7 +134,7 @@ var htlivesight = {
   About: function() {
 //	  alert("startup:About begin");
       var features = "chrome,titlebar,toolbar,centerscreen,modal,resizable";
-      var url = htlivesightEnv.contentPath+"about.html";
+      var url = htlivesightEnv.contentPath+"about.xul";
       window.openDialog(url, "About", features);
   //    alert("startup:About end");
   },
