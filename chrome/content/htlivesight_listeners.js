6b7553e8d8f77ae36f95350c13a877ceffa0c982
biigpapy@gmail.com <biigpapy@gmail.com@f9553a1c-787c-c2ac-8fa1-bce0d6f3e609>
2012-10-07 21:04:48 +0000

    restored manifest 1.0 (I have to move all the js out from html before upgrade manifest to 2.0 version).

---
+++ chrome/content/htlivesight_listeners.js
@@ -0,0 +1,9 @@
+htlivesightPrefs.init();
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  //document.querySelector('button').addEventListener('click', clickHandler); /***** example ****//
+	htlivesight.Settings.startup();
+	htlivesight.Lang.localization();
+});
+
