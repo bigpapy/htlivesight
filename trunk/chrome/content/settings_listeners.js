7b97b0ae90d5d32bef1b421da71f9be4470b9a1c
biigpapy@gmail.com <biigpapy@gmail.com@f9553a1c-787c-c2ac-8fa1-bce0d6f3e609>
2012-10-12 20:59:11 +0000

    moved tab 1,2,3,4 javascript out of htlivesight.html.

--- trunk/chrome/content/settings_listeners.js
+++ trunk/chrome/content/settings_listeners.js
@@ -7,8 +7,43 @@ document.addEventListener('DOMContentLoaded', function () {
 	document.getElementById("light_theme").addEventListener('click',function(){htlivesight.Settings.click.switch_style(3);});
 	document.getElementById("livefox_theme").addEventListener('click',function(){htlivesight.Settings.click.switch_style(4);});
 
-	document.getElementById("openin_tab").addEventListener('click',function(){htlivesight.Settings.click.radopenin(true);;});
+	document.getElementById("openin_tab").addEventListener('click',function(){htlivesight.Settings.click.radopenin(true);});
 	document.getElementById("openin_window").addEventListener('click',function(){htlivesight.Settings.click.radopenin(false);});
-//	<input type="radio" id="openin_tab" name="radio" checked="checked" /><label for="openin_tab" id="label_openin_tab" onclick="htlivesight.Settings.click.radopenin(true);">In a new tab</label>
-//	<input type="radio" id="openin_window" name="radio" /><label for="openin_window" id="label_openin_window" onclick="htlivesight.Settings.click.radopenin(false);">In a new window</label>
+	document.getElementById("hattrickServer").addEventListener('change',function(){htlivesight.Settings.click.txtfixhattrickserver();});
+	
+	/** tab 2 */
+	document.getElementById("chkGetLeague").addEventListener('click',function(){htlivesight.Settings.click.chkgetleague();});
+	document.getElementById("chkGetLeagueWithin").addEventListener('click',function(){htlivesight.Settings.click.chkgetleaguewithin();});
+	document.getElementById("txtGetLeagueWithinHours").addEventListener('change',function(){htlivesight.Settings.click.txtfixleaguehours();});
+	
+	document.getElementById("chkGetFriends").addEventListener('click',function(){htlivesight.Settings.click.chkgetfriends();});
+	document.getElementById("chkGetFriendsWithin").addEventListener('click',function(){htlivesight.Settings.click.chkgetfriendswithin();});
+	document.getElementById("txtGetFriendsWithinHours").addEventListener('change',function(){htlivesight.Settings.click.txtfixfriendshours();});
+	
+	document.getElementById("chkHdrScorers").addEventListener('click',function(){htlivesight.Settings.click.chkhdrscorers();});
+	document.getElementById("chkHdrBooked").addEventListener('click',function(){htlivesight.Settings.click.chkhdrbooked();});
+	document.getElementById("chkHdrSentOff").addEventListener('click',function(){htlivesight.Settings.click.chkhdrsentoff();});
+	document.getElementById("chkHdrInjured").addEventListener('click',function(){htlivesight.Settings.click.chkhdrinjured();});
+	
+	document.getElementById("txtMatchWindowSize").addEventListener('change',function(){htlivesight.Settings.click.txtfixwindowsize();});
+	
+	/** tab 3 */
+	document.getElementById("chkSound").addEventListener('click',function(){htlivesight.Settings.click.chkSound();});
+	document.getElementById("chkSoundOnlyOpened").addEventListener('click',function(){htlivesight.Settings.click.chkSoundOnlyOpened();});
+	document.getElementById("chkFlash").addEventListener('click',function(){htlivesight.Settings.click.chkFlash();});
+	document.getElementById("chkSlider").addEventListener('click',function(){htlivesight.Settings.click.chkSlider();});
+	
+	/** tab 4 */
+	document.getElementById("removeauth").addEventListener('click',function(){htlivesight.Settings.click.resetToken();});
+	document.getElementById("reverseOrder").addEventListener('click',function(){htlivesight.Settings.click.reverseOrder();});
+	document.getElementById("printEventKey").addEventListener('click',function(){htlivesight.Settings.click.printEventKey();});
+	
+	/** tab 5 */
+	document.getElementById("oldIcons").addEventListener('click',function(){htlivesight.Settings.click.oldIcons();});
+	
+	document.getElementById("weather").addEventListener('click',function(){htlivesight.Settings.click.weather();});
+	document.getElementById("whistleTime").addEventListener('click',function(){htlivesight.Settings.click.whistleTime();});
+	document.getElementById("weatherSE").addEventListener('click',function(){htlivesight.Settings.click.weatherSE();});
+	document.getElementById("livefoxGoal").addEventListener('click',function(){htlivesight.Settings.click.livefoxGoal();});
+	document.getElementById("noOpGoal").addEventListener('click',function(){htlivesight.Settings.click.noOpGoal();});
 });
