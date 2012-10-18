804b867d5bcca36b1a787ce407a8f41b5d0f1424
biigpapy@gmail.com <biigpapy@gmail.com@f9553a1c-787c-c2ac-8fa1-bce0d6f3e609>
2012-10-18 05:22:05 +0000

    fixed resize for firefox version. Chrome version doesn't working fine.

--- trunk/chrome/content/htlivesight_listeners.js
+++ trunk/chrome/content/htlivesight_listeners.js
@@ -1,13 +1,15 @@
 /* beginning script */
 htlivesightPrefs.init();
-
 document.addEventListener('DOMContentLoaded', function () {
 	htlivesight.Settings.startup();
 	htlivesight.Lang.localization();
 	htlivesight.startup();
+	document.body.onresize=function(){htlivesight.dynresize();};
+	
 	/* ending script */
 	/* beginning listeners */
-	document.getElementById("body").addEventListener('resize',function(){htlivesight.dynresize();});
+
+	//document.getElementById("body").addEventListener('resize',function(){htlivesight.dynresize();});
 	document.getElementById("chat_link").addEventListener('click',function(){htlivesight.chatdelay();});
 	document.getElementById("button_login").addEventListener('click',function(){htlivesight.Click.Login();});
 	document.getElementById("reLive").addEventListener('click',function(){htlivesight.reLive();});
