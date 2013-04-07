d4aecc5835259c96f3c3d96735eff29fd64f43b2
biigpapy@gmail.com <biigpapy@gmail.com@f9553a1c-787c-c2ac-8fa1-bce0d6f3e609>
2013-04-07 13:57:29 +0000

    - Added specialties to lineup and in copy to clipboard. - Fixed links about youth players. - Fixed link effect under chrome. - Added localization to specialties.

--- trunk/chrome/content/players.js
+++ trunk/chrome/content/players.js
@@ -1,58 +1,94 @@
 htlivesight.players = {
 
 };
-htlivesight.Player = function Team(id, firstName, lastName, specialty) {
-	console.log("a1");
+htlivesight.Player = function Team(id, firstName, lastName, specialty, teamId, youth) {
 	this.id = id;
-	console.log("a2");
 	this.firstName = firstName;
-	console.log("a3");
 	this.lastName = lastName;
-	console.log("a4");
 	this.specialty = specialty;
-	console.log("a5");
+	this.teamId = teamId;
+	this.youth = youth;
+
+};
+htlivesight.Player.List = new Object();
+htlivesight.players.HTTPGet = function (playerId,youth) {
+	if(htlivesight.Player.List["_"+playerId+"_"+youth].specialty!="") return;
+	if(youth=="false"){
+
+	var parameters=[["file","playerdetails"],
+	                ["version", "2.4"],
+	                ["playerID",playerId],
+	                ];
+	}else{
+
+		var parameters=[["file","youthplayerdetails"],
+		                ["version", "1.0"],
+		                ["youthPlayerId",playerId],
+		                ];
+			
+	}
+
+	htlivesight.ApiProxy.retrieve(document, parameters, function(xml){htlivesight.players.ParseGet(xml,playerId, youth);});
+
+};
+htlivesight.players.ParseGet = function(xml,playerId, youth){
+	//	var firstName = xml.getElementsByTagName("FirstName")[0].textContent;
+
+	//	var lastName = xml.getElementsByTagName("LastName")[0].textContent;
+
+//		var playerId;
+//		if(youth=="true"){
+//			playerId = xml.getElementsByTagName("YouthPlayerID")[0].textContent;
+//		}else{
+//			playerId = xml.getElementsByTagName("PlayerID")[0].textContent;	
+//		}
+
+		var specialty = xml.getElementsByTagName("Specialty")[0].textContent;
+		if(specialty == "" && youth=="true")specialty="0"; 
+
+	//	var player = new htlivesight.Player(playerId, firstName, lastName, specialty, teamId, youth);
+
+		htlivesight.Player.List["_"+playerId+"_"+youth].specialty = specialty;
+
+		if(parseInt(specialty)>=0 && parseInt(specialty)<=6){
+		  htlivesight.players.addSpecialtyToDom(playerId,youth,specialty);
+		}
+
+};
+htlivesight.players.specialtyChar = function (specialty){
+
+	switch (specialty){
+	case "0": return "";
+	break;
+	case "1": return "["+htlivesight.Util.Parse("Technical",htlivesight.data[0])+"] ";
+	break;
+	case "2": return "["+htlivesight.Util.Parse("Quick",htlivesight.data[0])+"] ";
+	break;
+	case "3": return "["+htlivesight.Util.Parse("Powerful",htlivesight.data[0])+"] ";
+	break;
+	case "4": return "["+htlivesight.Util.Parse("Unpredictable",htlivesight.data[0])+"] ";
+	break;
+	case "5": return "["+htlivesight.Util.Parse("Header",htlivesight.data[0])+"] ";
+	break;
+	case "6": return "["+htlivesight.Util.Parse("Regainer",htlivesight.data[0])+"] ";
+	break;
+	default: return "";
+	};
 };
 
-htlivesight.players.HTTPGet = function (teamId,sourceSystem) {return;
-	console.log("b1 sourceSystem ="+sourceSystem+" teamId= "+teamId);
-	youth = ((sourceSystem == "youth" || sourceSystem == "Youth" ) ? "True" : "False");
-	console.log("b2");
-	if ((youth == "True") || window.htlivesight.Live.PlayerList["_"+teamId+"_"+youth] )return;
-	console.log("b3");
-	var parameters=[["file","players"],
-	                ["version", "2.2"],
-	                ["teamID",teamId],
-	                ];
-	console.log("b4");
-	htlivesight.ApiProxy.retrieve(document, parameters, function(xml){htlivesight.players.ParseGet(xml, teamId, youth);});
-	console.log("b5");
-};
-htlivesight.players.ParseGet = function(xml, teamId, youth){
-	console.log("c1");
-	var playerList = {};
-	console.log("c2");
-	var playerNodes = xml.getElementsByTagName("Player");
-	console.log("c3");
-	for(var j = 0, len = playerNodes.length; j< len ;j++){
-		console.log("c4");
-		var playerNode = playerNodes[j];
-		console.log("c5");
-		var firstName = playerNode.getElementsByTagName("FirstName")[0].textContent;
-		console.log("c6");
-		var lastName = playerNode.getElementsByTagName("LastName")[0].textContent;
-		console.log("c7");
-		var playerId = playerNode.getElementsByTagName("PlayerID")[0].textContent;
-		console.log("c8");
-		var specialty = playerNode.getElementsByTagName("Specialty")[0].textContent;
-		console.log("c9");
-		console.log(" firstName= "+ firstName + " lastName= "+ lastName + " playerId= "+ playerId + " specialty= " + specialty);
-		var player = new htlivesight.Player(playerId, firstName, lastName, specialty);
-		console.log("c10");
-		playerList["_"+playerId] = player;
-		console.log("c11");
+htlivesight.players.addSpecialtyToDom = function (playerId, youth, specialty){
+
+	var playerClass;
+
+	if(youth.toLowerCase()=="true"){
+		playerClass = ""+playerId+"_youth";
+	}else{
+		playerClass = ""+playerId;
 	}
-	console.log("c12");
-	window.htlivesight.Live.PlayerList["_"+teamId+"_"+youth] = playerList;
-	console.log("c13");
-//	alert("end loading players from xml!");
+	$("."+playerClass).each(function() {
+		if(!$(this).hasClass("withSpecialty")){
+			$(this).text($(this).text()+" "+htlivesight.players.specialtyChar(specialty));
+			$(this).addClass("withSpecialty");
+		}
+	});
 };
